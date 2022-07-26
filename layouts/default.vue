b13bc4ce6e7a97c1d447a80def210f6e9bca3c96
kuas1091104337 <kuas1091104337@gmail.com>
2022-07-26 15:53:38 +0800

    7/26 update

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  setup(){
+    return { };
+  },
+}
+</script>
+
+<template>
+  <div>
+    <header><h3>header</h3></header>
+    <Nuxt />
+    <footer><h3>footer</h3></footer>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  setup(){
+    return { };
+  },
+}
+</script>
+
+<template>
+  <h2>about</h2>
+</template>
+
+<style lang="scss">
+
+</style>
